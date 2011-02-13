c69a16372ba0bdba7815b34e8c8d60f2e9791f7d
whosaysni <whosaysni@gmail.com>
2011-02-13 12:45:13 +0000

    introduced builtin ja_JP ihelp.

---
+++ ihelpbundle_ja_JP__this/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name = "ihelpbundle_ja_JP__this",
+    version = "0.1",
+    packages = find_packages(),
+
+    author = 'Yasushi Masuda',
+    author_email = 'whosaysni@gmail.com',
+    description = 'ihelp bundle for builtin functions.',
+    license = 'BSD',
+    keyword = 'help docstring pydoc i18n translation',
+    url = 'http://code.google.com/p/python-ihelp/',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        ]
+    )
