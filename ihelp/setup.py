b75789322f331f4b4def83383d2c9dfea748088a
whosaysni <whosaysni@gmail.com>
2011-02-16 02:16:44 +0000

    0.2.1 setup.py.

--- ihelp/setup.py
+++ ihelp/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "ihelp_",
-    version = "0.2",
+    version = "0.2.1",
     packages = find_packages(),
 
     author = 'Yasushi Masuda',
