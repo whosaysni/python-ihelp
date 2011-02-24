767264c3e98050ecc3667bee2b06ea3a8fb6140a
whosaysni <whosaysni@gmail.com>
2011-02-24 08:05:29 +0000

    Avoiding version anbiguity of builtin bundle.

--- ihelpbundle_ja_JP__py26/setup.py
+++ ihelpbundle_ja_JP__py26/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 setup(
-    name = "ihelpbundle_ja_JP__this",
-    version = "0.1.7",
+    name = "ihelpbundle_ja_JP__py26",
+    version = "0.1.8",
     packages = find_packages(),
 
     author = 'Yasushi Masuda',
     author_email = 'whosaysni@gmail.com',
-    description = 'ihelp bundle for builtin functions.',
+    description = 'ihelp bundle for Python 2.6 builtin/standard libraries.',
     license = 'BSD',
     keyword = 'help docstring pydoc i18n translation',
     url = 'http://code.google.com/p/python-ihelp/',
